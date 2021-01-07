52534886e32f17eb6af74d2bcd67c6c506e4068e
Ian McKain <ian@Ians-iMac.local>
2021-01-06 19:26:10 -0500

    add header and footer to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Project 3</p>
+      <Footer />
     </div>
   );
 }

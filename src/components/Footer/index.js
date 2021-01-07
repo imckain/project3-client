8c779179ba38c01a6c7b64239ef424b9e7c8a9c9
Ian McKain <ian@Ians-iMac.local>
2021-01-06 19:53:08 -0500

    add styles to header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css'
+
 const Footer = (props) => (
-    <footer>
-        Footer
+    <footer className={styles.Footer}>
+        <p>Copyright &copy; All rights reserved {new Date().getFullYear()} Project 3</p>
     </footer>
 )
 
